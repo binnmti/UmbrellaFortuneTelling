b5bd800ed2a7842dffe99f6a6e1294ddcd372930
Bin Matsui <matsui-bin@codeer.co.jp>
2016-06-28 03:58:40 +0900

    パターン増加

--- Applilcation/UmbrellaFortuneTelling/Web/Controllers/HomeController.cs
+++ Applilcation/UmbrellaFortuneTelling/Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -25,6 +26,27 @@ namespace Web.Controllers
             });
             return View();
         }
+
+
+
+        //ToDo リファクタリング対象
+        public class PictureData
+        {
+            public string Auther { get; set; }
+            public string FileName { get; set; }
+            public string Link { get; set; }
+        }
+        List<PictureData> PictureDatas = new List<PictureData>()
+        {
+            new PictureData() { Auther = "David Photo Studio",FileName = "Need.png", Link = "https://www.flickr.com/photos/jackal1203/14801855910/"},
+            new PictureData() { Auther = "gregt99", FileName = "Need2.png", Link = "https://www.flickr.com/photos/80592618@N06/16118194721/in/set-72157649551014970/"},
+            new PictureData() { Auther = "gregt99", FileName = "Need3.png", Link = "https://www.flickr.com/photos/80592618@N06/15527469693/in/set-72157647693591113"},
+            new PictureData() { Auther = "胖胖豬", FileName = "Need4.png", Link = "https://www.flickr.com/photos/nengwei/12132068234/in/set-72157640133070716/"},
+            new PictureData() { Auther = "swanky", FileName = "Need5.png", Link = "https://www.flickr.com/photos/swanky-hsiao/936676913/in/set-72157601074343374/"},
+            new PictureData() { Auther = "胖胖豬", FileName = "Need6.png", Link = "https://www.flickr.com/photos/nengwei/14189370508/in/set-72157645069854082/"},
+            new PictureData() { Auther = "しゃれこーべ", FileName = "Need7.png", Link = "https://www.flickr.com/photos/fragileruins/16457665672/"},
+        };
+
         public ActionResult Fortune(string value, WeatherReportData data)
         {
             if (data.City == null)
@@ -37,8 +59,13 @@ namespace Web.Controllers
             var model = report.TodayWeatherData();
             var um = report.GetUmbrella(DateTime.Now);
             ViewBag.Umbrella = um.Is;
-            ViewBag.Umbrella = um.Is;
             ViewBag.Percent = um.Percent + @"%";
+
+            var r = new Random();
+            var picture = PictureDatas[r.Next(PictureDatas.Count)];
+            ViewBag.FileName = picture.FileName;
+            ViewBag.Link = picture.Link;
+            ViewBag.Author = picture.Auther;
             return View(model);
         }
 
